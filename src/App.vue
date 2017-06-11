4bb0626a5b20cec7cdbe7d3b15f9efc553816459
wxin <wxking921@qq.com>
2017-06-11 23:09:20 +0800

    基于vue2.0实现cnode社区

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <vheader></vheader>
+    <router-view></router-view>
+    <vfooter></vfooter>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header'
+  import footer from './components/footer/footer'
+export default {
+  name: 'app',
+  data() {
+    return {
+
+    };
+  },
+  components: {
+      vheader:header,
+      vfooter:footer,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,36 @@
+<template>
+<header>
+	<div class="search">
+		<div class="search-icon"></div>
+	</div>
+</header>
+</template>
+
+<script>
+	export default{
+
+	}
+</script>
+<style lang="scss" scoped>
+	header{
+		position: fixed;
+		height: 1.4rem;
+		width:100%;
+		background: #747474;
+	
+	 .search{
+		    width: 80%;
+		    height: 1rem;
+	     	background: #eee;
+	     	position: absolute;
+	     	left: 50%;
+	     	top: 50%;
+	     	transform:translate(-55%,-50%);
+	     	.search-icon{
+	     		width:1rem;
+	     		height:1rem;
+	     		background: #fff;
+	     	}
+	  }
+	}
+</style>
